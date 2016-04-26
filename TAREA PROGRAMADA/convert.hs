b5caff3f31b8dcc6943c00aea8b1f317d57be607
Mario Naranjo <mario15695@hotmail.com>
2016-04-26 12:50:05 -0600

    Hola

--- TAREA PROGRAMADA/convert.hs
+++ TAREA PROGRAMADA/convert.hs
@@ -9,6 +9,49 @@ import Data.List
 import Data.Maybe
 
 
+l1 = [["a1","True","4"],["a2","False","2"],["a3","False","3"],["a4","True","4"]]
+l2 = [["2","True","azul","no"],["2","False","blanco","se"],["3","True","rojo","se"],["4","False","azul","so"]]
+
+
+prueba :: [[String]] -> [[String]] -> [[String]]
+prueba [] [] = []
+prueba lista1 [] = lista1
+prueba [] lista2 = lista2
+prueba (lista1 : resto1) (lista2 : resto2)
+  | lista1 !! 2 == lista2 !! 0 = [lista1 ++ lista2] ++ (prueba resto1 resto2)
+  | lista1 !! 2 /= lista2 !! 0 = (prueba [lista1] resto2) ++ [lista2] 
+
+
+
+
+
+
+main :: IO()
+main = do 
+    --Leer el archivo
+    inh <- openFile "R1.csv" ReadMode
+    inpStr <- hGetContents inh
+    --Obteniendo el nombre de las columnas y tos tipos de las columnas
+    let archivoList = splitOn "\n" inpStr
+    let nombreColumnas = splitOn ";" (archivoList !! 0)
+    let tiposColumnas = splitOn ";" (archivoList !! 1)
+    --Obteniendo las filas
+    let filasAux = snd (splitAt 2 archivoList)
+    let filas = init (map (splitOn ";") filasAux)
+
+    print $ nombreColumnas
+    print $ elemIndex "C" nombreColumnas
+    print $ filas
+
+
+
+
+
+
+
+
+
+
 {-
 main1 = do 
        putJValue(convertCSV_to_JSON ["A","B","C","D","E"] ["N","X","B","N","X"]  [["10","hola mundo","True","1.2","e1"], ["12","arena y sol","False","1.2","e1"], ["34","hola mundo","True","1.2","e1"]])
@@ -52,38 +95,6 @@ loadCSV_to_JSON fileName = do
        let json = convertCSV_to_JSON nombreColumnas tiposColumnas filas
 
        return (json)
-
-
-
-
-
-main :: IO()
-main = do 
-    --Leer el archivo
-    inh <- openFile "R2.csv" ReadMode
-    inpStr <- hGetContents inh
-    --Obteniendo el nombre de las columnas y tos tipos de las columnas
-    let archivoList = splitOn "\n" inpStr
-    let nombreColumnas = splitOn ";" (archivoList !! 0)
-    let tiposColumnas = splitOn ";" (archivoList !! 1)
-    --Obteniendo las filas
-    let filasAux = snd (splitAt 2 archivoList)
-    let filas = init (map (splitOn ";") filasAux)
-
-    print $ nombreColumnas
-    print $ elemIndex "C" nombreColumnas
-    print $ filas
-
-
-      
-
-
-
-
-
-
-
-
 
 
 -- **********************************************************************
